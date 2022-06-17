4f793bbb67e47539873bf3ffa05714756e9397d5
Ma-aruf M. Burad <burad.m@AD.SPROBE.PH>
2022-06-17 15:25:35 +0800

    add new resource

--- CRUD_APP/Database.py
+++ CRUD_APP/Database.py
@@ -29,35 +29,40 @@ class Student(Database):
         sql = "SELECT * FROM students ORDER BY id {}".format(mode)
 
         try:
-            my_cursor.execut(sql)
+            my_cursor.execute(sql)
             result = my_cursor.fetchall()
             return result
-
         except Exception as e:
             return e
 
-    def insert(self, data):
-        sql = ("INSERT INTO students (name, roll, address) VALUES (%s, %s, %s)")
+    def delete(self, id):
+        sql = "DELETE FROM students WHERE id = {}".format(id)
+        try:
+            my_cursor.execute(sql)
+            print(f"DELETED!")
+        except Exception as e:
+            raise e
 
+    def insert(self, *data):
+        sql = ("INSERT INTO students (name, roll, address) VALUES (%s, %s, %s)")
         try:
             my_cursor.execute(sql, (data))
             print(f'Executed!')
         except Exception as e:
             raise e
-            # return e
 
         return my_cursor.lastrowid
 
     def update(self, id, data):
         sql = "UPDATE students SET name = %s, roll = %s, address = %s WHERE id = {}".format(id)
-
         val = (data[0], data[1], data[2])
-
         try:
-            my_cursor.execute()
-
+            my_cursor.execute(sql, val)
+            print(f"UPDATED!!!")
         except Exception as e:
             return e
+
+
 
     def truncate(self):
         sql = "TRUNCATE TABLE students"
--- CRUD_APP/app.py
+++ CRUD_APP/app.py
@@ -1,4 +1,6 @@
 from Database import Database, Student
+import json
+import time
 
 DB_NAME = "python_mysql"
 
@@ -14,8 +16,18 @@ def main():
     st = Student(db_config)
 
     insert_sql = ("INSERT INTO students(`name`, `roll`, `address`) VALUES(`Ma-aaruf`, 505, 'india')")
+    name = "Ma-aruf"
+    roll = 505
+    address = "Pagadian City"
+    data = (name,roll, address)
 
-    st.insert(insert_sql)
+    for idx in range(0, 5):
+        st.insert(name, roll, address)
+        time.sleep(0.6)
+
+    students_list = st.all_students()
+    print(students_list)
+    # st.update(1,data)
     print(f"test")
 
 if __name__ == "__main__":
